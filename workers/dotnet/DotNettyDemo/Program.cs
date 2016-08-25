8a6004c2b265662a834fb1064b86da22d4f07248
Asım Arslan <arslanasim@gmail.com>
2016-08-25 18:49:02 +0300

    WIP

---
+++ workers/dotnet/DotNettyDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotNettyDemo
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+
+			var task = ServerConnector.RunServerAsync();
+
+			Console.WriteLine(task.Status);
+			ClientConnector.RunClientAsync().Wait();
+		}
+	}
+}
