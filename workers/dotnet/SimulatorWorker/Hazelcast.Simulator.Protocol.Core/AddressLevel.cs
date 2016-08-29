220e8f1d9b90c51e110f882a06c8190075b66663
Asım Arslan <arslanasim@gmail.com>
2016-08-29 10:47:08 +0300

    WIP

--- workers/dotnet/SimulatorWorker/Hazelcast.Simulator.Protocol.Core/AddressLevel.cs
+++ workers/dotnet/SimulatorWorker/Hazelcast.Simulator.Protocol.Core/AddressLevel.cs
@@ -1,5 +1,35 @@
-﻿namespace Hazelcast.Simulator.Protocol.Core
+﻿using log4net.Appender;
+namespace Hazelcast.Simulator.Protocol.Core
 {
+	/**
+	 * Defines the address level of a Simulator component.
+	 *
+	 * <pre>
+	 *                                               +---+
+	 * REMOTE                                        + R +
+	 *                                               +---+
+	 *                                                 |
+	 *                                                 v
+	 *                                               +---+
+	 * COORDINATOR           +-----------------------+ C +----------------------+
+	 *                       |                       +---+                      |
+	 *                       |                                                  |
+	 *                       v                                                  v
+	 *                    +--+---+                                           +---+--+
+	 * AGENT              | C_A1 |                              +------------+ C_A2 +------------+
+	 *                    +--+---+                              |            +------+            |
+	 *                       |                                 |                                 |
+	 *                       v                                 v                                 v
+	 *                  +----+----+                       +----+----+                       +----+----+
+	 * WORKER       +---+ C_A1_W1 +---+               +---+ C_A2_W1 +---+               +---+ C_A2_W2 +---+
+	 *              |   +---------+   |               |   +---------+   |               |   +---------+   |
+	 *              |                 |               |                 |               |                 |
+	 *              v                 v               v                 v               v                 v
+	 *        +-----+------+   +------+-----+   +-----+------+   +------+-----+   +-----+------+   +------+-----+
+	 * TEST   | C_A1_W1_T1 |   | C_A1_W1_T2 |   | C_A2_W1_T1 |   | C_A2_W1_T2 |   | C_A2_W2_T1 |   | C_A2_W2_T2 |
+	 *        +------------+   +------------+   +------------+   +------------+   +------------+   +------------+
+	 * </pre>
+	 */
 	public enum AddressLevel
 	{
 		REMOTE=-1,
@@ -11,7 +41,22 @@
 
 	public static class AddressLevelUtil
 	{
-		
+		public static int GetMinLevel(this AddressLevel al)
+		{
+			return (int)AddressLevel.REMOTE;
+		}
+
+		public static int GetMaxLevel(this AddressLevel al)
+		{
+			return (int)AddressLevel.TEST;
+		}
+
+		public static bool IsParentAddressLevel(this AddressLevel al, AddressLevel addressLevel)
+		{
+			return (int)al < (int)addressLevel;
+		}
+
+
 	}
 }
 
