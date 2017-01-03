178324dd992e7fd1362bfd0c10188d20d2faf48e
Asım Arslan <arslanasim@gmail.com>
2017-01-03 16:51:00 +0300

    WIP

--- workers/dotnet/SimulatorWorker.Tests/Hazelcast.Simulator.Utils/HazelcastUtilTest.cs
+++ workers/dotnet/SimulatorWorker.Tests/Hazelcast.Simulator.Utils/HazelcastUtilTest.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net;
+using Hazelcast.Core;
+using Moq;
+using NUnit.Framework;
 
 namespace Hazelcast.Simulator.Utils
 {
-    class HazelcastUtilTest
+    [TestFixture]
+    public class HazelcastUtilTest
     {
+        private const string ADDRESS = "127.0.0.1";
+        private Mock<IHazelcastInstance> hzClient;
+
+        [SetUp]
+        public void Setup()
+        {
+            hzClient = new Mock<IHazelcastInstance>();
+            var iClient = new Mock<IEndpoint>();
+            iClient.Setup(t => t.GetSocketAddress()).Returns(new IPEndPoint(IPAddress.Parse(ADDRESS), 5701));
+            hzClient.Setup(client => client.GetLocalEndpoint()).Returns(iClient.Object);
+        }
+
+        [TearDown]
+        public void TearDown() {}
+
+        [Test]
+        public void TestGetAddress_hzNull()
+        {
+            string theAddress = HazelcastUtils.GetHazelcastAddress("dotnetclient", ADDRESS, null);
+            Assert.AreEqual($"client:{ADDRESS}", theAddress);
+        }
+
+        [Test]
+        public void TestGetAddress_hzMock()
+        {
+            string theAddress = HazelcastUtils.GetHazelcastAddress("dotnetclient", ADDRESS, hzClient.Object);
+            Assert.AreEqual("127.0.0.1:5701", theAddress);
+        }
     }
 }
